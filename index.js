2e51769b0477a9034118ce074f5427a614146a63
Connor <101947931+CARhoden@users.noreply.github.com>
2023-07-29 19:43:17 -0500

    added questions

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs/promises');
+
+async function questions() {
+    await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'What text would you like in your logo? (Up to three letters)'
+            },
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'What color would you like the text to be?',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'What shape would you like your logo to be?',
+                choices: ['Circle', 'Triangle', 'Square']
+            },
+            {
+                type: "input",
+                name: 'shapeColor',
+                message: 'What color would you like the shape to be?',
+            }
+        ])
+}
+
+questions();
