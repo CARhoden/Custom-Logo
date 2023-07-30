559495cdabd8e542d7f2f13c51b5b10f402e3364
Connor <101947931+CARhoden@users.noreply.github.com>
2023-07-29 21:15:37 -0500

    added the logo generating function

--- index.js
+++ index.js
@@ -26,7 +26,30 @@ async function questions() {
                 name: 'shapeColor',
                 message: 'What color would you like the shape to be?',
             }
-        ])
+        ]).then((answers) => {
+            if (answers.text.length > 3) {
+                console.log('Error: Text can not be longer than 3 letters, please try again.');
+                main();
+                return
+            }
+            fs.writeFile(`./examples/${answers.text + answers.shapeColor}.svg`, generateLogo(answers), err =>
+                err ? console.log('Uh-oh, something went wrong.. try again') : console.log('Generated logo.svg'));
+            console.log('Generated logo.svg');
+        });
 }
+
+function generateLogo(answers) {
+    if (answers.shape === 'Square') {
+        var shape = new Square(answers.shapeColor).render()
+    } else if (answers.shape === 'Triangle') {
+        var shape = new Triangle(answers.shapeColor).render()
+    } else {
+        var shape = new Circle(answers.shapeColor).render()
+    }
+    return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+    ${shape}
+    <text x="150" y="115" font-size="40" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+    </svg>`
+};
 
 questions();
